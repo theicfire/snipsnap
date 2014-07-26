e04660524420c737695393a82d6304c688e25d66
Max Song <maxsong@Mystery.local>
2014-07-26 15:50:33 -0700

    got rss feed going on

--- server/server.js
+++ server/server.js
@@ -6,11 +6,16 @@ Meteor.startup(function () {
 		update_stuff: function () {
 			console.log('refresh!');
 			//GET "https://www.kimonolabs.com/api/9bfmpq4s?apikey=4f4ec5d7768e5516b71c7ba5c69b786d"
-			HTTP.get('https://www.kimonolabs.com/api/9bfmpq4s',{apikey:'4f4ec5d7768e5516b71c7ba5c69b786d'},
+			HTTP.get('https://www.kimonolabs.com/api/9bfmpq4s?apikey=4f4ec5d7768e5516b71c7ba5c69b786d',{},
 				function (error,result) {
-					console.log(result);
+					
+					var results = JSON.parse(result.content).results.collection1;
+					
+					for (var i = 0; i < results.length; i++) {
+						console.log('hello there');
+						console.log(results[i]['Article Title'].href);
+					}
 				});
-
 		}
 	});
 
