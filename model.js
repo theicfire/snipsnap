4e317540898e9103173c64c44b6eb547d2b160dd
Max Song <maxsong@Mystery.local>
2014-07-26 17:16:47 -0700

    more stuff

--- model.js
+++ model.js
@@ -1,3 +1,9 @@
 console.log('both places');
 
-posts = new Meteor.Collection('posts');
+Snippet = new Meteor.Collection('snippets'); 
+// {_id, user_path, href, title, text, founder}
+// Unique is <founder, href, text>
+ 
+User = new Meteor.Collection('users'); 
+//{ _id, user_id, snippet_id}
+// Unique is <user_id, snippet_id>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 ////server
 console.log("im the server too");
 
+var xxhash = Meteor.require('xxhash');
+
 Meteor.startup(function () {
 	Meteor.methods({
 		update_stuff: function () {
@@ -12,8 +14,16 @@ Meteor.startup(function () {
 					var results = JSON.parse(result.content).results.collection1;
 					
 					for (var i = 0; i < results.length; i++) {
-						console.log('hello there');
+// {_id, user_path, href, title, text, founder}
+
+						// console.log('hello there');
 						console.log(results[i]['Article Title'].href);
+						var title = results[i]['Article Title'].text;
+						var href = results[i]['Article Url'].href;
+						var text = results[i]['Actual Text'].text;
+						var totalString = title+href+text;
+						// console.log(XXHash.hash(totalString, 0xCAFEBABE));
+
 					}
 				});
 		}
